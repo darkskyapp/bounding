e554308c9d3d26038dabbb8c7505921a8a528421
J. T. L <ironwallaby@gmail.com>
2017-05-17 09:25:42 -0400

    Switch to ES6

--- bounding.js
+++ bounding.js
@@ -1,115 +1,80 @@
-(function() {
-  "use strict";
-  var box, circle;
+"use strict";
 
-  box = function(p) {
-    var box, i, x;
+function bounding_box(points) {
+  const aabb = [+Infinity, +Infinity, -Infinity, -Infinity];
+  for(const [x, y] of points) {
+    if(x < aabb[0]) aabb[0] = x;
+    if(y < aabb[1]) aabb[1] = y;
+    if(x > aabb[2]) aabb[2] = x;
+    if(y > aabb[3]) aabb[3] = y;
+  }
+  return aabb;
+}
 
-    box = [
-      Number.POSITIVE_INFINITY,
-      Number.POSITIVE_INFINITY,
-      Number.NEGATIVE_INFINITY,
-      Number.NEGATIVE_INFINITY
-    ];
+function bounding_convex_hull(points) {
+  /* FIXME */
+}
 
-    for(i = 0; i < p.length; i++) {
-      x = p[i];
+function _circle_1([x, y]) {
+  return [x, y, 0];
+}
 
-      box[0] = Math.min(box[0], x[0]);
-      box[1] = Math.min(box[1], x[1]);
-      box[2] = Math.max(box[2], x[0]);
-      box[3] = Math.max(box[3], x[1]);
-    }
+function _circle_2([ax, ay], [bx, by]) {
+  const x = (bx - ax) / 2;
+  const y = (by - ay) / 2;
+  return [ax + x, ay + y, Math.hypot(x, y)];
+}
 
-    return box;
-  };
+function _circle_3([ax, ay], [bx, by], [cx, cy]) {
+  const mx = bx - ax;
+  const my = by - ay;
+  const nx = cx - ax;
+  const ny = cy - ay;
 
-  circle = (function() {
-    var circle, circle1, circle2, circle3, does_not_contain;
-    
-    circle1 = function(a) {
-      return [a[0], a[1], 0];
-    };
+  const m = mx * mx + my * my;
+  const n = nx * nx + ny * ny;
+  const d = (mx * ny - my * nx) * 2;
+  const x = (ny * m - my * n) / d;
+  const y = (mx * n - nx * m) / d;
+  return [ax + x, ay + y, Math.hypot(x, y)];
+}
 
-    circle2 = function(a, b) {
-      var x, y;
+function _contains([cx, cy, cr], [px, py]) {
+  const x = cx - px;
+  const y = cy - py;
+  return x * x + y * y <= cr * cr;
+}
 
-      x = 0.5 * (b[0] - a[0]);
-      y = 0.5 * (b[1] - a[1]);
-
-      return [a[0] + x, a[1] + y, Math.sqrt(x * x + y * y)];
-    };
-
-    circle3 = function(a, b, c) {
-      var as, ax, ay, bs, bx, by, d, r, x, y;
-
-      ax = b[0] - a[0];
-      ay = b[1] - a[1];
-      as = ax * ax + ay * ay;
-      bx = c[0] - a[0];
-      by = c[1] - a[1];
-      bs = bx * bx + by * by;
-
-      d = 0.5 / (ax * by - ay * bx);
-      x = d * (by * as - ay * bs);
-      y = d * (ax * bs - bx * as);
-
-      return [a[0] + x, a[1] + y, Math.sqrt(x * x + y * y)];
-    };
-
-    does_not_contain = function(c, p) {
-      var r, x, y;
-
-      x = c[0] - p[0];
-      y = c[1] - p[1];
-      r = c[2];
-      return x * x + y * y > r * r;
-    };
-
-    circle = function(p, r) {
-      var d, x;
-
-      if(r.length === 3) {
-        d = circle3(r[0], r[1], r[2]);
-      }
-
-      else if(p.length === 0 && r.length === 2) {
-        d = circle2(r[0], r[1]);
-      }
-
-      else if(p.length === 0 && r.length === 1) {
-        d = circle1(r[0]);
-      }
-
-      else if(p.length === 1 && r.length === 0) {
-        d = circle1(p[0]);
-      }
-
-      else {
-        x = p.pop();
-        d = circle(p, r);
-        if(does_not_contain(d, x)) {
-          r.push(x);
-          d = circle(p, r);
-          r.pop();
-        }
-        p.push(x);
-      }
-
-      return d;
-    };
-
-    return function(p) {
-      return circle(p, []);
-    };
-  })();
-
-  if(typeof exports !== "undefined") {
-    exports.box    = box;
-    exports.circle = circle;
+function _bounding_circle(p, q) {
+  if(q.length === 3) {
+    return _circle_3(q[0], q[1], q[2]);
+  }
+  if(p.length === 0 && q.length === 2) {
+    return _circle_2(q[0], q[1]);
+  }
+  if(p.length === 0 && q.length === 1) {
+    return _circle_1(q[0]);
+  }
+  if(p.length === 1 && q.length === 0) {
+    return _circle_1(p[0]);
   }
 
-  else {
-    window.bounding = {box: box, circle: circle};
+  const x = p.pop();
+  let d = _bounding_circle(p, q);
+  if(!_contains(d, x)) {
+    q.push(x);
+    d = _bounding_circle(p, q);
+    q.pop();
   }
-})();
+  p.push(x);
+  return d;
+}
+
+function bounding_circle(points) {
+  return _bounding_circle(Array.from(points), []);
+}
+
+if(typeof exports !== "undefined") {
+  exports.box    = bounding_box;
+  exports.circle = bounding_circle;
+}
