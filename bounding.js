5bef5d28b70121b95339b5b215b09efa2ec4d59b
J. T. L <ironwallaby@gmail.com>
2017-05-19 13:05:53 -0400

    3.0.0: flattened pointsets

--- bounding.js
+++ bounding.js
@@ -1,27 +1,30 @@
 "use strict";
 
 function bounding_box(points) {
-  const aabb = [+Infinity, +Infinity, -Infinity, -Infinity];
-  for(const [x, y] of points) {
-    if(x < aabb[0]) aabb[0] = x;
-    if(y < aabb[1]) aabb[1] = y;
-    if(x > aabb[2]) aabb[2] = x;
-    if(y > aabb[3]) aabb[3] = y;
+  const box = [+Infinity, +Infinity, -Infinity, -Infinity];
+
+  for(let i = points.length; i--; ) {
+    const x = points[i];
+    const m = i & 1;
+    const n = m | 2;
+    if(x < box[m]) box[m] = x;
+    if(x > box[n]) box[n] = x;
   }
-  return aabb;
+
+  return box;
 }
 
-function _circle_1([x, y]) {
+function _circle_1(x, y) {
   return [x, y, 0];
 }
 
-function _circle_2([ax, ay], [bx, by]) {
+function _circle_2(ax, ay, bx, by) {
   const x = (bx - ax) / 2;
   const y = (by - ay) / 2;
   return [ax + x, ay + y, Math.hypot(x, y)];
 }
 
-function _circle_3([ax, ay], [bx, by], [cx, cy]) {
+function _circle_3(ax, ay, bx, by, cx, cy) {
   const mx = bx - ax;
   const my = by - ay;
   const nx = cx - ax;
@@ -35,34 +38,33 @@ function _circle_3([ax, ay], [bx, by], [cx, cy]) {
   return [ax + x, ay + y, Math.hypot(x, y)];
 }
 
-function _contains([cx, cy, cr], [px, py]) {
-  const x = cx - px;
-  const y = cy - py;
-  return x * x + y * y <= cr * cr;
-}
-
 function _bounding_circle(p, q) {
-  if(q.length === 3) {
-    return _circle_3(q[0], q[1], q[2]);
+  if(q.length === 6) {
+    return _circle_3(q[0], q[1], q[2], q[3], q[4], q[5]);
+  }
+  if(p.length === 0 && q.length === 4) {
+    return _circle_2(q[0], q[1], q[2], q[3]);
   }
   if(p.length === 0 && q.length === 2) {
-    return _circle_2(q[0], q[1]);
+    return _circle_1(q[0], q[1]);
   }
-  if(p.length === 0 && q.length === 1) {
-    return _circle_1(q[0]);
-  }
-  if(p.length === 1 && q.length === 0) {
-    return _circle_1(p[0]);
+  if(p.length === 2 && q.length === 0) {
+    return _circle_1(p[0], p[1]);
   }
 
+  const y = p.pop();
   const x = p.pop();
   let d = _bounding_circle(p, q);
-  if(!_contains(d, x)) {
-    q.push(x);
+  const a = x - d[0];
+  const b = y - d[1];
+  const c = d[2];
+  if(a * a + b * b > c * c) {
+    q.push(x, y);
     d = _bounding_circle(p, q);
     q.pop();
+    q.pop();
   }
-  p.push(x);
+  p.push(x, y);
   return d;
 }
 
@@ -70,44 +72,41 @@ function bounding_circle(points) {
   return _bounding_circle(Array.from(points), []);
 }
 
-function _lexicographically([ax, ay], [bx, by]) {
-  let t = ax - bx;
-  if(t === 0) {
-    t = ay - by;
-  }
-  return t;
-}
-
-function _cross([ax, ay], [bx, by], [cx, cy]) {
-  return (bx - ax) * (cy - ay) - (by - ay) * (cx - ax);
-}
-
 function bounding_hull(points) {
-  points = Array.from(points).sort(_lexicographically);
+  const hull = [];
+  const n = points.length >> 1;
 
-  const u = [];
-  for(let i = 0; i < points.length; i++) {
-    while(u.length >= 2 && _cross(points[i], u[u.length - 2], u[u.length - 1]) >= 0) {
-      u.pop();
+  let l = 0;
+  for(let i = 1; i < n; i++) {
+    if(points[i << 1] < points[l << 1]) {
+      l = i;
     }
-    u.push(points[i]);
   }
 
-  const l = [];
-  for(let i = points.length - 1; i >= 0; i--) {
-    while(l.length >= 2 && _cross(points[i], l[l.length - 2], l[l.length - 1]) >= 0) {
-      l.pop();
+  let p = l;
+  do {
+    const px = points[(p << 1) | 0];
+    const py = points[(p << 1) | 1];
+    hull.push(px, py);
+    if(hull.length > points.length) {
+      throw new Error("infinite loop");
     }
-    l.push(points[i]);
-  }
 
-  u.pop();
-  l.pop();
-  for(let i = 0; i < l.length; i++) {
-    u.push(l[i]);
-  }
+    let q = 0;
+    for(let i = 1; i < n; i++) {
+      const qx = points[(q << 1) | 0];
+      const qy = points[(q << 1) | 1];
+      const ix = points[(i << 1) | 0];
+      const iy = points[(i << 1) | 1];
+      if((q === p) || (iy - py) * (qx - ix) - (ix - px) * (qy - iy) < 0) {
+        q = i;
+      }
+    }
 
-  return u;
+    p = q;
+  } while(p !== l);
+
+  return hull;
 }
 
 if(typeof exports !== "undefined") {
